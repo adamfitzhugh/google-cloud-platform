9d69aa422e5ca2ff25558f4467e372d67e6fb9dc
root <adam@opti-lt-0044.localdomain>
2021-04-22 15:44:51 +0100

    Added repo

---
+++ terraform/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.65.0"
+    }
+  backend "remote" {
+    organization = "adamfitzhugh-github"
+
+    workspaces {
+      name = "google-cloud-platform"
+    }
+  }
+}
+
+provider "google" {
+  project     = var.project
+  region      = var.gcp_region
+}
